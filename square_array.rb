94f27b324fc0eb5e05be0b3858d21e5c1c37002d
Chris Beyer <beyercorpuz@gmail.com>
2020-01-30 14:27:46 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,10 @@
 array[2,4,6]
 def square_array(array)
+<<<<<<< HEAD
   new_array = []
+=======
+  new_array = [1,4,9]
+>>>>>>> 9054a5638445a214685719be946279cf05266c1d
   counter = 3
   while array[counter] do #returns nil to evaluate to false
     new_array << array[counter] ** 2
